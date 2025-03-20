95a084ff191519aea2c53674fcf8afaf76d93ad7
ljunn9 <147462254+ljunn9@users.noreply.github.com>
2025-03-20 02:40:20 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 FS = 250  
 
 ELECTRODE_INDICES = {
-    "SSVEP": [0, 1, 2], 
-    "P300": [3, 4, 5] 
+    "SSVEP": [0, 1, 2],  
+    "P300": [3, 4, 5, 6, 7]  
 }
+
+P300_ELECTRODES = ELECTRODE_INDICES["P300"]
+SSVEP_ELECTRODES = ELECTRODE_INDICES["SSVEP"]
 
 STIMULUS_POSITIONS = {
     "UP": {"pos": (400, 100), "freq": 6},
@@ -17,6 +20,7 @@ CURSOR_MOVEMENT = 20
 P300_FLASH_PROBABILITY = 0.2  
 P300_FLASH_DURATION = 0.1  
 P300_FLASH_INTERVAL = 0.3  
+
 
 P300_TARGETS = {
     "UP": {"pos": (400, 100)},
