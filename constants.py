a45d17c04842c604af1f4e93ee91b37f52c95cb2
ljunn9 <147462254+ljunn9@users.noreply.github.com>
2025-03-20 02:19:34 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,7 @@
 FS = 250  
-ELECTRODE_INDICES = [0, 1, 2]  
+
+SSVEP_ELECTRODE_INDICES = [0, 1, 2] 
+P300_ELECTRODE_INDICES = [3, 4, 5]  
 
 STIMULUS_POSITIONS = {
     "UP": {"pos": (400, 100), "freq": 6},
@@ -9,3 +11,13 @@ STIMULUS_POSITIONS = {
 }
 
 CURSOR_MOVEMENT = 20
+
+P300_FLASH_PROBABILITY = 0.2  
+P300_FLASH_DURATION = 0.1  
+P300_FLASH_INTERVAL = 0.3  
+
+P300_TARGETS = {
+    "UP": {"pos": (400, 100)},
+    "DOWN": {"pos": (400, 500)},
+    "LEFT": {"pos": (100, 300)},
+    "RIGHT": {"pos": (700, 300)}
