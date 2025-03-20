bd157791e84be434a106727cbb789ccb5a0f560d
ljunn9 <147462254+ljunn9@users.noreply.github.com>
2025-03-19 17:05:21 -0700

    defines constants

---
+++ constants.py
@@ -0,0 +1,11 @@
+FS = 250  
+ELECTRODE_INDICES = [0, 1, 2]  
+
+STIMULUS_POSITIONS = {
+    "UP": {"pos": (400, 100), "freq": 6},
+    "DOWN": {"pos": (400, 500), "freq": 8},
+    "LEFT": {"pos": (100, 300), "freq": 10},
+    "RIGHT": {"pos": (700, 300), "freq": 12}
+}
+
+CURSOR_MOVEMENT = 20
