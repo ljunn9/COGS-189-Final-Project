f36859324c7df4609f6aeae95bf100f3e3335475
ljunn9 <147462254+ljunn9@users.noreply.github.com>
2025-03-20 13:46:06 -0700

    Update stimulus.py

--- stimulus.py
+++ stimulus.py
@@ -1,43 +1,48 @@
 import time
 import pygame
 import random
+from pylsl import StreamInfo, StreamOutlet
 from config import STIMULUS_POSITIONS
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("SSVEP + P300 Stimuli")
 
+info = StreamInfo(name='Markers', type='Markers', channel_count=1, channel_format='int32', source_id='stimulus_markers')
+outlet = StreamOutlet(info)
+
 def flicker_stimuli(num_trials=20, trial_duration=5):
+    running = True
     clock = pygame.time.Clock()
     flicker_state = {key: True for key in STIMULUS_POSITIONS}
     event_timestamps = []
-    
-    trial_count = 0 
-    while trial_count < num_trials:  
+
+    for trial in range(num_trials):
+        print(f"🔹 Trial {trial + 1}/{num_trials} started")
         trial_start = time.time()
-        
-        while time.time() - trial_start < trial_duration:  
-            screen.fill((0, 0, 0))        
-            p300_target = random.choice(list(STIMULUS_POSITIONS.keys()))  
+
+        while time.time() - trial_start < trial_duration:
+            screen.fill((0, 0, 0))
+
+            p300_target = random.choice(list(STIMULUS_POSITIONS.keys()))
             event_timestamps.append((time.time(), p300_target))
+            outlet.push_sample([1])  
 
             for key, stim in STIMULUS_POSITIONS.items():
-                if flicker_state[key]:
-                    color = (255, 255, 255) if key != p300_target else (0, 255, 0)
-                    pygame.draw.circle(screen, color, stim["pos"], 50)
+                color = (255, 255, 255) if key != p300_target else (0, 255, 0)
+                pygame.draw.circle(screen, color, stim["pos"], 50)
 
                 if time.time() % (1 / stim["freq"]) < (1 / (2 * stim["freq"])):
-                    flicker_state[key] = not flicker_state[key]  
-                    
+                    flicker_state[key] = not flicker_state[key]
+
             pygame.display.flip()
             clock.tick(60)
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                    break
-        
-        trial_count += 1  
+                    pygame.quit()
+                    return event_timestamps
 
     pygame.quit()
     return event_timestamps
