d9228c2a22ee0a4d82e6a45f908fe071aa320c1f
ljunn9 <147462254+ljunn9@users.noreply.github.com>
2025-03-21 04:44:01 -0700

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ ELECTRODE_INDICES = {
 P300_ELECTRODES = ELECTRODE_INDICES["P300"]
 SSVEP_ELECTRODES = ELECTRODE_INDICES["SSVEP"]
 
-STIMULUS_POSITIONS = {
-    "UP": {"pos": (400, 100), "freq": 6},
-    "DOWN": {"pos": (400, 500), "freq": 8},
-    "LEFT": {"pos": (100, 300), "freq": 10},
-    "RIGHT": {"pos": (700, 300), "freq": 12}
-}
+STIMULUS_POSITIONS = [
+    ["Q", "W", "E", "R", "T", "Y"],
+    ["A", "S", "D", "F", "G", "H"],
+    ["Z", "X", "C", "V", "B", "N"],
+    ["1", "2", "3", "4", "5", "6"],
+]
 
 CURSOR_MOVEMENT = 20
 
